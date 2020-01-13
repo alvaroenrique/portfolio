38a9f409ae81c796a389b59a107d2c3c11e6b0b6
alvaroenrique <alvaroenriquehl@gmail.com>
2020-01-12 19:51:21 -0500

    Added index page, with first theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import BlogButton from '../components/button-links/blog';
+import PersonalProjectsButton from '../components/button-links/personal-projects';
+import ProffesionalWorkButton from '../components/button-links/professional-work';
+import ResumeButton from '../components/button-links/resume';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+const IndexPage = () => {
+  const [isSubtitleChange, setIsSubtitleChange] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsSubtitleChange(true);
+    }, 1500);
+  });
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h2 className="text-center primary-font text-lg">HI,</h2>
+      <h2
+        className={`subtitle text-center primary-font text-md ${
+          isSubtitleChange ? 'fade-out-br' : 'fade-in'
+        }`}
+      >
+        My name is Alvaro. I’m a frontend web developer
+      </h2>
+      {isSubtitleChange && (
+        <h2 className="subtitle text-center primary-font text-md fade-in-left">
+          Here is something about me
+        </h2>
+      )}
 
-    <h2 className="text-center primary-font text-lg">HI, test</h2>
-    <h2 className="text-center primary-font">My name is Alvaro. I’m a frontend web developer</h2>
-  </Layout>
-);
+      {isSubtitleChange && (
+        <>
+          <BlogButton />
+          <PersonalProjectsButton />
+          <ProffesionalWorkButton />
+          <ResumeButton />
+        </>
+      )}
+    </Layout>
+  );
+};
 
 export default IndexPage;
